1ee89f733b0813d0461040c1312d1f554acd4c69
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-08 16:35:24 -0700

    Create methods for Bookshelf Class

--- lib/app/models/bookshelf.rb
+++ lib/app/models/bookshelf.rb
@@ -1,4 +1,27 @@
 class Bookshelf < ActiveRecord::Base
     has_many :ratings
-    has_many :books, :foreign_key => :rating
-end
+    has_many :books
+    @@bookshelf =[]
+
+    def self.add_book(btitle, bauthor, bgenre = nil, best_seller = false, read= false, rating)
+      @@bookshelf << Book.create(:title => btitle, :author => bauthor, :genre => bgenre, :best_seller => best_seller, :read => read, :rating => rating)
+    end 
+
+    def self.my_books
+      @@bookshelf
+    end
+
+    def self.change_rating(book,newtitle)
+      cbook = Book.find{|key| key.title == book}
+      cbook.update(title: newtitle)
+      cbook
+    end
+
+    def self.sell_books
+      Book.destroy_all
+      puts "Made some cash!"
+    end
+
+  end
+
+  #:first_name => "Emilia" author
