03845a3574b850ab494fde9d5bc9d28cc6663639
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-10 10:44:39 -0700

    Add ReadME

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,30 @@ Book.destroy_all
 Bookshelf.destroy_all 
 Genre.destroy_all 
 
-# create 5 books
+jackson = Bookshelf.create(:name => "Jackson")
+rita = Bookshelf.create(:name => "Rita")
+
+# create 5 books for Jackson's shelf
 5.times do
   book_title = Faker::Book.title
   book_author = Faker::Book.author
   book_genre = Faker::Book.genre
   g1 =Genre.create(name:Faker::Book.genre)
-  bs =Bookshelf.create(name:Faker::Name.name)
-  Book.create(title:book_title, author:book_author, genre_id:g1.id, bookshelf_id:bs.id)
+  #bs =Bookshelf.create(name:Faker::Name.name)
+  Book.create(title:book_title, author:book_author, genre_id:g1.id, bookshelf_id:jackson.id)
 end 
+
+
+# create 5 books for Rita's shelf
+5.times do
+  book_title = Faker::Book.title
+  book_author = Faker::Book.author
+  book_genre = Faker::Book.genre
+  g1 =Genre.create(name:Faker::Book.genre)
+  #bs =Bookshelf.create(name:Faker::Name.name)
+  Book.create(title:book_title, author:book_author, genre_id:g1.id, bookshelf_id:rita.id)
+end 
+
 
 
 # run rake db:seed once fix genre thing
