eb803b52b931db305214aaf1492cc82eb1cb6162
Margarita Morozova <ritamorozova@yahoo.com>
2020-09-09 19:46:24 -0700

    Update rita branch with master's

--- lib/app/models/bookshelf.rb
+++ lib/app/models/bookshelf.rb
@@ -1,36 +1,6 @@
 class Bookshelf < ActiveRecord::Base
     has_many :books
     has_many :genres, through: :books 
-    
-
-  
-    # def drake_made_this
-    #   if Artist.all.find{|a| a.name == "Drake"}== nil
-    #     drake = Artist.create(name: "Drake")
-    #   end
-    #   self.artist = Artist.all.find{|a| a.name == "Drake"}
-
-
-
-
-   
-    # def self.update_book(book,newtitle)
-    #   cbook = Book.find{|key| key.title == book}
-    #   cbook.update(title: newtitle)
-    #   cbook
-    # end
-
-    # def self.sell_books
-    #   Book.destroy_all
-    #   puts "Made some cash!"
-    # end
-
-    # def self.delete_by(title)
-    #   Book.where(title: title).destroy_all
-    # end 
-
   end
 
-  #:first_name => "Emilia" author
-  #def self.add_book(btitle, bauthor, bgenre = nil, best_seller = false, read= false, rating
-  # :author => bauthor, :genre => bgenre, :best_seller => best_seller, :read => read, :rating => rating
+ 
--- lib/app/models/genre.rb
+++ lib/app/models/genre.rb
@@ -2,7 +2,4 @@ class Genre < ActiveRecord::Base
     has_many :books
     has_many :bookshelves, through: :books
 
-    def self.new_genre(genre_name)
-        ngenre = Genre.create(:name => genre_name)
-    end
 end
